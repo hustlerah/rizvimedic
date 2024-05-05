6a3cd4dd8057b13fd395e83c94da2fee6328941a
hustlerah <153553054+hustlerah@users.noreply.github.com>
2024-05-05 18:49:16 +0500

    Add files via upload

---
+++ public/assets/data/services.js
@@ -0,0 +1,44 @@
+export const services = [
+  {
+    name: "Cancer Care",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+    num:'1'
+  },
+  {
+    name: "Labor & Delivery",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+    num:'2'
+  },
+  {
+    name: "Heart & Vascular",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+    num:'3'
+  },
+  {
+    name: "Mental Health",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+    num:'4'
+  },
+  {
+    name: "Neurology",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+    num:'5'
+  },
+  {
+    name: "Burn Treatment",
+    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+    num:'6'
+  },
+];
